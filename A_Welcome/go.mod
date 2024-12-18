871411b36c069cc2dee0f097c6b5bec2f669ee17
VIRENDRA KATE <102298880+Viru9029@users.noreply.github.com>
2024-12-18 18:29:00 +0530

    Welcome code

---
+++ A_Welcome/go.mod
@@ -0,0 +1,3 @@
+module github.com/Viru9029/GoLang-Learning/A_Welcome
+
+go 1.21.6
---
+++ A_Welcome/main.go
@@ -0,0 +1,8 @@
+// Welcome to a tour of the Go programming language.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
